79a2fa0247557ed8c0405882897734fb200a3964
PatriciaDivi <patriciavianad@gmail.com>
2023-10-30 22:08:50 -0300

    build(DESENVOLVIMENTO):

---
+++ src/components/GistImages.tsx
@@ -0,0 +1,270 @@
+'use client'
+
+import { makeRequest } from "@/lib/api";
+import { Key } from "lucide-react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+type NameGistProps = {
+  nameGist: [];
+
+}
+
+type GistProps = {
+  id: number;
+  url: string;
+  html_url: string;
+  description: string;
+  files: {
+    [key:string]: {
+      raw_url: string;
+      filename: string;
+      language: string;
+    }
+  }
+
+}
+
+type FilesProps = {
+  files: {
+    [key:string]: {
+      raw_url: string;
+      filename: string;
+      language: string;
+    }
+  }
+}
+
+export function GistImages({nameGist}: NameGistProps) {
+  const [gistElement, setGistElement] = useState<GistProps[]>([]);
+  const [filterFiles, setFilterFiles] = useState<any>([]);
+  const [test, setTest] = useState<any>([]);
+
+  const dataFiles:any = [
+    {
+      "url": "https://api.github.com/gists/d1e50eadcf048eb4b82f34ee5632f966",
+      "forks_url": "https://api.github.com/gists/d1e50eadcf048eb4b82f34ee5632f966/forks",
+      "commits_url": "https://api.github.com/gists/d1e50eadcf048eb4b82f34ee5632f966/commits",
+      "id": "d1e50eadcf048eb4b82f34ee5632f966",
+      "node_id": "G_kwDOAkskRdoAIGQxZTUwZWFkY2YwNDhlYjRiODJmMzRlZTU2MzJmOTY2",
+      "git_pull_url": "https://gist.github.com/d1e50eadcf048eb4b82f34ee5632f966.git",
+      "git_push_url": "https://gist.github.com/d1e50eadcf048eb4b82f34ee5632f966.git",
+      "html_url": "https://gist.github.com/patriciadivi/d1e50eadcf048eb4b82f34ee5632f966",
+      "files": {
+        "gistfile1.txt": {
+          "filename": "gistfile1.txt",
+          "type": "text/plain",
+          "language": "Text",
+          "raw_url": "https://gist.githubusercontent.com/patriciadivi/d1e50eadcf048eb4b82f34ee5632f966/raw/01b59a8c201755a705b401de23d2d67ce67a37c4/gistfile1.txt",
+          "size": 16720
+        }
+      },
+      "public": true,
+      "created_at": "2023-08-30T20:23:35Z",
+      "updated_at": "2023-09-19T23:25:19Z",
+      "description": "habits.svg",
+      "comments": 0,
+      "user": null,
+      "comments_url": "https://api.github.com/gists/d1e50eadcf048eb4b82f34ee5632f966/comments",
+      "owner": {
+        "login": "patriciadivi",
+        "id": 38478917,
+        "node_id": "MDQ6VXNlcjM4NDc4OTE3",
+        "avatar_url": "https://avatars.githubusercontent.com/u/38478917?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/patriciadivi",
+        "html_url": "https://github.com/patriciadivi",
+        "followers_url": "https://api.github.com/users/patriciadivi/followers",
+        "following_url": "https://api.github.com/users/patriciadivi/following{/other_user}",
+        "gists_url": "https://api.github.com/users/patriciadivi/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/patriciadivi/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/patriciadivi/subscriptions",
+        "organizations_url": "https://api.github.com/users/patriciadivi/orgs",
+        "repos_url": "https://api.github.com/users/patriciadivi/repos",
+        "events_url": "https://api.github.com/users/patriciadivi/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/patriciadivi/received_events",
+        "type": "User",
+        "site_admin": false
+      },
+      "truncated": false
+    },
+    {
+      "url": "https://api.github.com/gists/53963f8bffd9b44359c400c085b4f92e",
+      "forks_url": "https://api.github.com/gists/53963f8bffd9b44359c400c085b4f92e/forks",
+      "commits_url": "https://api.github.com/gists/53963f8bffd9b44359c400c085b4f92e/commits",
+      "id": "53963f8bffd9b44359c400c085b4f92e",
+      "node_id": "G_kwDOAkskRdoAIDUzOTYzZjhiZmZkOWI0NDM1OWM0MDBjMDg1YjRmOTJl",
+      "git_pull_url": "https://gist.github.com/53963f8bffd9b44359c400c085b4f92e.git",
+      "git_push_url": "https://gist.github.com/53963f8bffd9b44359c400c085b4f92e.git",
+      "html_url": "https://gist.github.com/patriciadivi/53963f8bffd9b44359c400c085b4f92e",
+      "files": {
+        "image.svg": {
+          "filename": "image.svg",
+          "type": "image/svg+xml",
+          "language": "SVG",
+          "raw_url": "https://gist.githubusercontent.com/patriciadivi/53963f8bffd9b44359c400c085b4f92e/raw/20c126e324dd57477109644107db9c9aa2002fac/image.svg",
+          "size": 232
+        }
+      },
+      "public": true,
+      "created_at": "2023-07-13T03:11:34Z",
+      "updated_at": "2023-10-04T18:25:37Z",
+      "description": "image.svg",
+      "comments": 0,
+      "user": null,
+      "comments_url": "https://api.github.com/gists/53963f8bffd9b44359c400c085b4f92e/comments",
+      "owner": {
+        "login": "patriciadivi",
+        "id": 38478917,
+        "node_id": "MDQ6VXNlcjM4NDc4OTE3",
+        "avatar_url": "https://avatars.githubusercontent.com/u/38478917?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/patriciadivi",
+        "html_url": "https://github.com/patriciadivi",
+        "followers_url": "https://api.github.com/users/patriciadivi/followers",
+        "following_url": "https://api.github.com/users/patriciadivi/following{/other_user}",
+        "gists_url": "https://api.github.com/users/patriciadivi/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/patriciadivi/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/patriciadivi/subscriptions",
+        "organizations_url": "https://api.github.com/users/patriciadivi/orgs",
+        "repos_url": "https://api.github.com/users/patriciadivi/repos",
+        "events_url": "https://api.github.com/users/patriciadivi/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/patriciadivi/received_events",
+        "type": "User",
+        "site_admin": false
+      },
+      "truncated": false
+    },
+    {
+      "url": "https://api.github.com/gists/7f65fd88d1c9a8011d1596ac28e1e448",
+      "forks_url": "https://api.github.com/gists/7f65fd88d1c9a8011d1596ac28e1e448/forks",
+      "commits_url": "https://api.github.com/gists/7f65fd88d1c9a8011d1596ac28e1e448/commits",
+      "id": "7f65fd88d1c9a8011d1596ac28e1e448",
+      "node_id": "G_kwDOAkskRdoAIDdmNjVmZDg4ZDFjOWE4MDExZDE1OTZhYzI4ZTFlNDQ4",
+      "git_pull_url": "https://gist.github.com/7f65fd88d1c9a8011d1596ac28e1e448.git",
+      "git_push_url": "https://gist.github.com/7f65fd88d1c9a8011d1596ac28e1e448.git",
+      "html_url": "https://gist.github.com/patriciadivi/7f65fd88d1c9a8011d1596ac28e1e448",
+      "files": {
+        "memorie.svg": {
+          "filename": "memorie.svg",
+          "type": "image/svg+xml",
+          "language": "SVG",
+          "raw_url": "https://gist.githubusercontent.com/patriciadivi/7f65fd88d1c9a8011d1596ac28e1e448/raw/89aac82592497086daa756fcf866ea8d729cb254/memorie.svg",
+          "size": 4078898
+        },
+        "noMemorie.svg": {
+          "filename": "noMemorie.svg",
+          "type": "image/svg+xml",
+          "language": null,
+          "raw_url": "https://gist.githubusercontent.com/patriciadivi/7f65fd88d1c9a8011d1596ac28e1e448/raw/028c05a5da1ba007c6c5f32ffd5c7be3ac0879b0/noMemorie.svg",
+          "size": 315933
+        }
+      },
+      "public": true,
+      "created_at": "2023-07-05T04:12:33Z",
+      "updated_at": "2023-07-05T04:18:29Z",
+      "description": "noMemorie555",
+      "comments": 0,
+      "user": null,
+      "comments_url": "https://api.github.com/gists/7f65fd88d1c9a8011d1596ac28e1e448/comments",
+      "owner": {
+        "login": "patriciadivi",
+        "id": 38478917,
+        "node_id": "MDQ6VXNlcjM4NDc4OTE3",
+        "avatar_url": "https://avatars.githubusercontent.com/u/38478917?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/patriciadivi",
+        "html_url": "https://github.com/patriciadivi",
+        "followers_url": "https://api.github.com/users/patriciadivi/followers",
+        "following_url": "https://api.github.com/users/patriciadivi/following{/other_user}",
+        "gists_url": "https://api.github.com/users/patriciadivi/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/patriciadivi/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/patriciadivi/subscriptions",
+        "organizations_url": "https://api.github.com/users/patriciadivi/orgs",
+        "repos_url": "https://api.github.com/users/patriciadivi/repos",
+        "events_url": "https://api.github.com/users/patriciadivi/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/patriciadivi/received_events",
+        "type": "User",
+        "site_admin": false
+      },
+      "truncated": false
+    },
+    {
+      "url": "https://api.github.com/gists/31432328af375086fb1985655d9bdbb6",
+      "forks_url": "https://api.github.com/gists/31432328af375086fb1985655d9bdbb6/forks",
+      "commits_url": "https://api.github.com/gists/31432328af375086fb1985655d9bdbb6/commits",
+      "id": "31432328af375086fb1985655d9bdbb6",
+      "node_id": "G_kwDOAkskRdoAIDMxNDMyMzI4YWYzNzUwODZmYjE5ODU2NTVkOWJkYmI2",
+      "git_pull_url": "https://gist.github.com/31432328af375086fb1985655d9bdbb6.git",
+      "git_push_url": "https://gist.github.com/31432328af375086fb1985655d9bdbb6.git",
+      "html_url": "https://gist.github.com/patriciadivi/31432328af375086fb1985655d9bdbb6",
+      "files": {
+        "cardpatriciadivi.svg": {
+          "filename": "cardpatriciadivi.svg",
+          "type": "image/svg+xml",
+          "language": "SVG",
+          "raw_url": "https://gist.githubusercontent.com/patriciadivi/31432328af375086fb1985655d9bdbb6/raw/a58eb77695a69a0dcbcf35780a76365f680fc447/cardpatriciadivi.svg",
+          "size": 331711
+        }
+      },
+      "public": true,
+      "created_at": "2022-06-19T09:37:54Z",
+      "updated_at": "2022-06-19T09:43:33Z",
+      "description": "",
+      "comments": 0,
+      "user": null,
+      "comments_url": "https://api.github.com/gists/31432328af375086fb1985655d9bdbb6/comments",
+      "owner": {
+        "login": "patriciadivi",
+        "id": 38478917,
+        "node_id": "MDQ6VXNlcjM4NDc4OTE3",
+        "avatar_url": "https://avatars.githubusercontent.com/u/38478917?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/patriciadivi",
+        "html_url": "https://github.com/patriciadivi",
+        "followers_url": "https://api.github.com/users/patriciadivi/followers",
+        "following_url": "https://api.github.com/users/patriciadivi/following{/other_user}",
+        "gists_url": "https://api.github.com/users/patriciadivi/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/patriciadivi/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/patriciadivi/subscriptions",
+        "organizations_url": "https://api.github.com/users/patriciadivi/orgs",
+        "repos_url": "https://api.github.com/users/patriciadivi/repos",
+        "events_url": "https://api.github.com/users/patriciadivi/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/patriciadivi/received_events",
+        "type": "User",
+        "site_admin": false
+      },
+      "truncated": false
+    },
+  ]
+
+  async function handleFilterFiles() {
+    // const files = await gistElement?.filter((element:any) => (element.files) && element)
+    // setTest(files)
+    console.log('----> gistElement', gistElement)
+  }
+
+  async function fetchGistElement() {
+    const gist = await makeRequest('patriciadivi/gists', 'get') // GistImages retorna uma Promise<Element>
+    setGistElement(gist);
+  }
+
+  useEffect(() => {
+    fetchGistElement();
+    console.log('entrei aqui');
+    
+    // handleFilterFiles();
+
+  }, []);
+  // console.log('----> filterFiles', test)
+  // const gist: GistProps[] = await makeRequest('patriciadivi/gists', 'get')
+
+  return (
+    <>
+      {/* <p>{ `${nameGist.toLocaleLowerCase()}.svg` }</p> */}
+      
+       
+        
+    </>
+  )
+}
---
+++ src/components/repositories/FilterTechnology.tsx
@@ -0,0 +1,448 @@
+'use client'
+
+import React, { MouseEvent, useState } from "react";
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
+import { ValidateImage } from "../ValidateImage";
+import Link from "next/link";
+import { GistImages } from "../GistImages";
+
+  type ReposProps = {
+  name: string;
+  full_name: string;
+  id: number;
+  visibility: string;
+  created_at: string;
+  git_url: string;
+  html_url: string;
+  language: string;
+  description: string;
+  topics: string[];
+}
+
+type GistProps = {
+  id: number;
+  url: string;
+  html_url: string;
+  description: string;
+  files: {
+    [key:string]: {
+      raw_url: string;
+      filename: string;
+      language: string;
+    }
+  }
+
+}
+
+type FilesProps = {
+  files: {
+    [key:string]: {
+      raw_url: string;
+      filename: string;
+      language: string;
+    }
+  }
+}
+
+  interface Propslist {
+    list: ReposProps[]
+    gists: GistProps[]
+  }
+
+
+export function FilterTechnology({ list, gists }:Propslist) {
+  const [search, setSearch] = useState('')
+
+  const listRepos = list
+  const listGists = gists
+
+  const filteredRepos = (!search || search !== 'All') 
+  ? listRepos.filter((repo) => repo.topics.includes(search) )
+  : [];
+
+  const filteredNames = listRepos?.map((repo) => repo.name )
+  const filteredGists = listGists?.map((repo) => repo.files )
+  const filteredImages = filteredGists?.filter((gist, index) => {
+    return Object.keys(gist).some((file)=>{
+      // const data = filteredNames.filter((nameImage) => ({
+      //   filename: `${file}.svg` === `${nameImage}.svg`
+      // }))
+      // const { filename, raw_url } = file
+      // const array = []
+      // return array.push(filename, raw_url)
+      console.log('---> file', file)
+      return (file === "newPatriciaDivi.svg") && file
+    })
+  })
+  console.log('----> filteredNames',filteredNames);
+  console.log('----> gists', filteredGists);
+  console.log('----> images', filteredImages);
+  
+
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    const text = event.currentTarget.textContent;
+    // console.log('----> text', text?.toLocaleLowerCase())
+
+    if (text) {
+      setSearch(text?.toLocaleLowerCase());
+    }
+
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSearch(event.target.value.toLowerCase())
+    console.log("Selected Value:", event.target.value.toLowerCase());
+  }
+  
+  // console.log('---> renderizei', search)
+  
+  return (
+    <>
+    {
+      // Mobile
+      (search === '' || search === 'all') ? (
+                          
+        <section className="mb-8 w-11/12">
+        {/* title | busca */}
+        <div 
+          className="flex items-center justify-between sm:pl-0 sm:flex-col sm:items-start"
+        >
+          <h1 
+            className="font-bold sm:text-5xl text-3xl mr-4 sm:mb-[-1px]"
+          > Meus 
+            <span className="text-[#00ADB5]"> Projetos</span>
+          </h1>
+
+          <div className="flex sm:gap-2 max-sm:hidden">
+
+            <button
+              type="button" 
+              className={`${(search === '' || search === 'all') 
+              ? "bg-[#00ADB5] border-current" 
+              : "bg-[#393E46]"} bg-opacity-50 py-3 px-9 rounded-3xl my-4 border border-transparent focus:bg-[#00ADB5] focus:border focus:border-current transition-shadow `}
+              value='all'
+              onClick={ ({currentTarget: { value }}) => setSearch(value)}
+            >
+              All
+            </button>
+            
+            <button 
+              className="bg-[#393E46] bg-opacity-50 py-3 px-9 rounded-3xl my-4 border border-transparent focus:bg-[#00ADB5] focus:border focus:border-current transition-shadow"
+              value='front-end'
+              onClick={ ({currentTarget: { value }}) => setSearch(value)}
+            >
+                Front-end
+            </button>
+            
+            <button 
+              className="bg-[#393E46] bg-opacity-50 py-3 px-9 rounded-3xl my-4 border border-transparent focus:bg-[#00ADB5] focus:border focus:border-current transition-shadow"
+              value='back-end'
+              onClick={ ({currentTarget: { value }}) => setSearch(value)}
+            >
+                Back-end
+            </button>
+            
+            <button 
+              className="bg-[#393E46] bg-opacity-50 py-3 px-9 rounded-3xl my-4 border border-transparent focus:bg-[#00ADB5] focus:border focus:border-current transition-shadow"
+              value='full-stack'
+              onClick={ ({currentTarget: { value }}) => setSearch(value)}
+            >
+                Full Stack
+            </button>
+          </div>
+          
+          <select 
+            name="technology" 
+            id="technology" 
+            value={ search }
+            onChange={ handleChange }
+            className="py-2 px-4 rounded-lg bg-[#00ADB5] sm:hidden"
+          >
+            <option value="all" className="text-center">All</option>
+            <option value="front-end">Front-end</option>
+            <option value="back-end">Back-end</option>
+            <option value="full-stack">Full-stack</option>
+          </select>
+          
+
+        </div>
+
+        {/* container card */}
+        <div 
+          className="sm:flex sm:flex-row sm:flex-wrap sm:border sm:border-violet-500 sm:justify-around md:space-between md:items-center md:mx-auto gap-6 mt-11 xl:max-w-7xl"
+        >
+            
+          {/* container card */}
+
+        
+          {/* <div>
+            < GistImages nameGist={filteredNames}/>
+          </div> */}
+          
+          {
+            
+            listRepos.map((repo, index) => {
+              const date = dayjs(repo.created_at).locale('pt-br').format('MMM/YYYY')
+
+              return (
+              <div
+              key={`${index}-${repo.name}`} 
+              className="mb-10 bg-[#393E46] bg-opacity-50 rounded-xl sm:min-h-[350px] sm:w-[368px] shadow-lg duration-300 dark:hover:brightness-50 hover:brightness-150 hover:scale-105"
+            >
+
+              <Link   
+                href={repo.html_url} 
+                target="_blank"
+                className="flex flex-col items-center gap-2 p-4"
+              >
+                    
+                <div className="flex items-center justify-end w-[102%] text-xs text-[#00ADB5]  sm:w-[104%]">
+                  <p>{date.toUpperCase()}</p>
+                </div>
+
+                <div 
+                  className="w-[90%] h-[230px] p-2 bg-[url('https://source.unsplash.com/500x300/?code')] bg-cover bg-center sm:w-[95%] sm:h-[150px]"
+                />
+
+
+                <div 
+                  className="flex flex-col justify-between items-start w-[90%] sm:w-[95%]"
+                >
+
+                  <div 
+                    className="flex items-center justify-between w-full"
+                  >
+
+                    <h2 className="text-xl mb-2 text-[#00ADB5] sm:text-2xl">
+                      { repo.name.charAt(0).toUpperCase() + repo.name.slice(1) }
+                    </h2>
+
+                    <span className="flex items-center gap-1">
+                      <ValidateImage listTechs={repo.topics}/>
+                     
+                    </span>
+                  </div>
+
+                  {
+                    (repo.description) ? (
+                      <p className="tracking-wider leading-6 text-white text-base text-left sm:text-left sm:text-sm"> 
+                        { repo.description }
+                      </p>
+                     ) : (
+                      <p className="tracking-wider leading-6 text-white text-base text-left  border border-violet-600  sm:text-sm"> 
+                        Descrição ainda não preenchida
+                      </p>
+                     )
+                  }
+
+                  
+                  
+                  
+                </div>
+
+                </Link>
+              </div>
+
+              )
+            })
+          }
+
+
+        </div>   
+
+        </section>
+
+      ) :
+      (
+                                
+        <section className="mb-8 w-11/12">
+        {/* title | busca */}
+        <div 
+          className="flex items-center justify-between sm:pl-0 sm:flex-col sm:items-start"
+        >
+          <h1 
+            className="font-bold sm:text-5xl text-3xl mr-4 sm:mb-[-1px]"
+          > Meus
+            <span className="text-[#00ADB5]"> Projetos</span>
+          </h1>
+
+          <div className="flex sm:gap-2 max-sm:hidden">
+
+            <button
+              type="button"
+              className={`${(search === '' || search === 'all') && "bg-[#00ADB5]"} bg-[#393E46] bg-opacity-50 py-3 px-9 rounded-3xl my-4 border border-transparent focus:bg-[#00ADB5] focus:border focus:border-current transition-shadow`}
+              value='all'
+              onClick={ ({currentTarget: { value }}) => setSearch(value)}
+            >
+              All
+            </button>
+            
+            <button 
+              className="bg-[#393E46] bg-opacity-50 py-3 px-9 rounded-3xl my-4 border border-transparent focus:bg-[#00ADB5] focus:border focus:border-current transition-shadow"
+              value='front-end'
+              onClick={ ({currentTarget: { value }}) => setSearch(value)}
+            >
+                Front-end
+            </button>
+            
+            <button 
+              className="bg-[#393E46] bg-opacity-50 py-3 px-9 rounded-3xl my-4 border border-transparent focus:bg-[#00ADB5] focus:border focus:border-current transition-shadow"
+              value='back-end'
+              onClick={ ({currentTarget: { value }}) => setSearch(value)}
+            >
+                Back-end
+            </button>
+            
+            <button 
+              className="bg-[#393E46] bg-opacity-50 py-3 px-9 rounded-3xl my-4 border border-transparent focus:bg-[#00ADB5] focus:border focus:border-current transition-shadow"
+              value='full-stack'
+              onClick={ ({currentTarget: { value }}) => setSearch(value)}
+            >
+                Full Stack
+            </button>
+          </div>
+          
+          <select 
+            name="technology" 
+            id="technology" 
+            value={ search }
+            onChange={ handleChange }
+            className="py-2 px-4 rounded-lg bg-[#00ADB5] sm:hidden"
+          >
+            <option value="all" className="text-center">All</option>
+            <option value="front-end">Front-end</option>
+            <option value="back-end">Back-end</option>
+            <option value="full-stack">Full-stack</option>
+          </select>
+          
+
+        </div>
+
+        {/* container card */}
+        <div 
+          className="sm:flex sm:flex-row sm:flex-wrap sm:border sm:border-violet-500 sm:justify-around md:space-between md:items-center md:mx-auto gap-6 mt-11 xl:max-w-7xl"
+        >
+            
+          {/* container card */}
+          {
+            (filteredRepos.length > 0) ? (
+
+              filteredRepos.map((repo, index) => {
+                const date = dayjs(repo.created_at).locale('pt-br').format('MMM/YYYY')
+
+                return (
+                <div
+                key={`${index}-${repo.name}`} 
+                className="mb-10 bg-[#393E46] bg-opacity-50 rounded-xl sm:min-h-[350px] sm:w-[368px] shadow-lg duration-300 dark:hover:brightness-50 hover:brightness-150 hover:scale-105"
+              >
+
+                <Link   
+                  href={repo.html_url} 
+                  target="_blank"
+                  className="flex flex-col items-center gap-2 p-4"
+                >
+                      
+                  <div className="flex items-center justify-end w-[102%] text-xs text-[#00ADB5]  sm:w-[104%]">
+                    <p>{date.toUpperCase()}</p>
+                  </div>
+
+                  <div 
+                    className="w-[90%] h-[230px] p-2 bg-[url('https://source.unsplash.com/800x600/?code')] bg-cover bg-center sm:w-[95%] sm:h-[150px]"
+                  />
+
+                  {
+                    (index >= 0) && (
+                      <img 
+                          src="https://source.unsplash.com/random/800x600/?code" 
+                          alt="" 
+                          className="h- w-24 sm:h-24 sm:w-24" 
+                        />
+                    )
+                  }
+                  <p>{index}</p>
+
+                  <div 
+                    className="flex flex-col justify-between items-start w-[90%] sm:w-[95%]"
+                  >
+
+                    <div 
+                      className="flex items-center justify-between w-full"
+                    >
+
+                      <h2 className="text-xl mb-2 text-[#00ADB5] sm:text-2xl">
+                        { repo.name.charAt(0).toUpperCase() + repo.name.slice(1) }
+                      </h2>
+
+
+
+
+
+                      <span
+                        key={`${repo.name}-${index}`}
+                        className="flex items-center gap-1"
+                      >
+                        <ValidateImage listTechs={repo.topics}/>
+                        {/* <img 
+                          src="https://cdn.icon-icons.com/icons2/1488/PNG/512/5352-html5_102567.png" 
+                          alt="" 
+                          className="h-7 w-7 sm:h-9 sm:w-9" 
+                        />
+
+                        <img 
+                          src="https://logospng.org/download/css-3/logo-css-3-1024.png" 
+                          alt="" 
+                          className="h-7 w-7 sm:h-8 sm:w-8" 
+                        />
+
+                        <img 
+                          src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" 
+                          alt="" 
+                          className="h-6 w-6 sm:h-7 sm:w-7" 
+                        /> */}
+                      </span>
+                    </div>
+
+                    {
+                      (repo.description) ? (
+                        <p className="tracking-wider leading-6 text-white text-base text-left sm:text-left sm:text-sm"> 
+                          { repo.description }
+                        </p>
+                      ) : (
+                        <p className="tracking-wider leading-6 text-white text-base text-left  border border-violet-600  sm:text-sm"> 
+                          Descrição ainda não preenchida
+                        </p>
+                      )
+                    }
+                    
+                  </div>
+
+                  </Link>
+                </div>
+
+                )
+              })
+
+            ) :
+            (
+             <>
+               <p className="border">não tem conteúdo</p>
+             </>
+            )
+
+          }
+
+
+        </div>   
+
+        </section>
+
+      )
+    }
+    
+    </>
+
+  );
+}
