a2fca6c881a7a9b599165059b015b4573abcbb50
PatriciaDivi <patriciavianad@gmail.com>
2024-02-26 21:34:14 -0300

    refactor(DESENVOLVIMENTO): :lipstick: Corrigindo estilo da página

--- src/app/contact/loading.tsx
+++ src/app/contact/loading.tsx
@@ -2,11 +2,12 @@ import { CardSkeleton } from "@/components/skeletons/CardSkeleton";
 
 export default function Loading(){
   return(
-    <div className="flex items-center justify-center min-h-screen bg-neutral-900 max-md:flex-col  md:space-x-4 ">
-      <div className="flex items-center justify-center gap-y-5 gap-x-8 sm:flex-wrap w-[900px] max-sm:flex-col">
-      <div className="h-5 w-1/3 bg-neutral-600 mb-4 bafore:-translate-x-full relative space-y-3 overflow-hidden rounded-md p-3 shadow before:absolute before:inset-0 before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg"></div>
-      <div className="h-5 w-1/4 bg-neutral-600 mb-4 bafore:-translate-x-full relative space-y-3 overflow-hidden rounded-md p-3 shadow before:absolute before:inset-0 before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg"></div>
-        <CardSkeleton/>
+    <div className="flex min-h-screen items-center justify-center bg-neutral-900">
+      <div className="flex flex-col items-center justify-center">
+      <div className="bafore:-translate-x-full relative mb-4 h-5 w-11/12 space-y-3 overflow-hidden rounded-md bg-neutral-600 p-3 shadow before:absolute before:inset-0 before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg"></div>
+
+      <div className="bafore:-translate-x-full relative mb-4 h-5 w-8/12 space-y-3 overflow-hidden rounded-md bg-neutral-600 p-3 shadow before:absolute before:inset-0 before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg"></div>
+      <CardSkeleton/>
       </div>
     </div>
   )
